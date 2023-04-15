8eb403a0b8808f96e4d6072c1d47837a51e5d26a
Alexandre <47799117+xandykati98@users.noreply.github.com>
2023-04-14 23:22:40 -0300

    correcao de bug nos tipos

--- src/pages/busca.tsx
+++ src/pages/busca.tsx
@@ -45,13 +45,13 @@ const Busca = () => {
         availableTipos: (tiposDistinct || []).map(({ tipo }: { tipo: string }) => tipo)
     }}>
         <div className="sm:grid-cols-[296px_1fr] relative mx-auto mt-16 grid w-full max-w-7xl px-4 sm:mt-20 sm:px-6 lg:px-8 xl:mt-32">
-            <div className="bg-gray-100 text-gray-600 flex flex-col items-center justify-center rounded-md py-4 px-6">
+            <div className="bg-gray-100 text-gray-600 flex flex-col items-baseline rounded-md py-4 px-6">
                 <div className="mb-2">O que você precisa?</div>
                 <div className="pointer-events-auto w-full justify-center flex divide-x divide-slate-400/20 rounded-md bg-white text-[1.0125rem] font-medium leading-5 text-slate-700 shadow-sm ring-1 ring-slate-700/10">
                     <div onClick={() => setTransacao('Comprar')} className={`${transacao === 'Comprar' ? 'bg-primary-600 hover:bg-primary-700 text-gray-50 hover:text-gray-50' : 'hover:bg-slate-50 hover:text-slate-900'} select-none rounded-r-none text-center rounded-md ring-primary-200 w-full px-6 py-3 cursor-pointer hover:ring-2 `}>Comprar</div>
                     <div onClick={() => setTransacao('Alugar')} className={`${transacao === 'Alugar' ? 'bg-primary-600 hover:bg-primary-700 text-gray-50 hover:text-gray-50' : 'hover:bg-slate-50 hover:text-slate-900'} select-none rounded-l-none text-center rounded-md ring-primary-200 w-full px-6 py-3 cursor-pointer hover:ring-2`}>Alugar</div>
                 </div>
-                <div className="bg-gray-200 h-[1px] w-full my-6"></div>
+                <div className="bg-gray-200 h-[1px] w-full my-3 mt-5"></div>
                 <div className="mb-2">Qual tipo?</div>
                 <Tipos/>
             </div>
@@ -110,18 +110,22 @@ function Tipos() {
             }, {}))
         }
     }
+    const actives: { [tipo:string]: true } = Object.entries(BuscaParams.tipos).filter(([key, value]) => value).reduce<{ [tipo:string]: true } >((acc:{ [tipo:string]: true } , [key, value]) => {
+        acc[key] = true
+        return acc
+    }, {})
     return (
         <Menu as="div" className="relative inline-block text-left w-full">
         <div className="w-full">
             <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
                 {
-                    Object.keys(BuscaParams.tipos).length === 0 ? 
+                    Object.keys(actives).length === 0 ? 
                     'Selecione um tipo' : 
-                    BuscaParams.availableTipos.length === Object.keys(BuscaParams.tipos).length 
+                    BuscaParams.availableTipos.length === Object.keys(actives).length 
                     ? 'Todos os imóveis' 
                     : <span>
-                        {TiposRelate[Object.keys(BuscaParams.tipos)[0] as 'Residential / Apartment']}
-                        {Object.keys(BuscaParams.tipos).length > 1 && <span className="text-gray-400"> + {Object.keys(BuscaParams.tipos).length - 1}</span>}
+                        {TiposRelate[Object.keys(actives)[0] as 'Residential / Apartment']}
+                        {Object.keys(actives).length > 1 && <span className="text-gray-400"> + {Object.keys(actives).length - 1}</span>}
                     </span>}
                 <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
             </Menu.Button>
@@ -141,7 +145,7 @@ function Tipos() {
                 <div onClick={toggleAll} className="cursor-pointer hover:bg-gray-100">
                     <a className="text-gray-700 px-4 py-2 text-md flex font-medium align-center">
                         <SwitchComp
-                        value={BuscaParams.availableTipos.length === Object.keys(BuscaParams.tipos).length}/>
+                        value={BuscaParams.availableTipos.length === Object.keys(actives).length}/>
                         <span className="ml-2">Todos os imóveis</span>
                         
                     </a>
